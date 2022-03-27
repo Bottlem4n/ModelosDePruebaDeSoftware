4db92dc29b5671e2890f9998f7ee904bc6ee934e
Jair Botello <jairbotello25@outlook.com>
2022-03-27 12:17:57 -0600

    corroborar fraccion

--- AppOperacionFraccion.java
+++ AppOperacionFraccion.java
@@ -2,7 +2,7 @@ public class AppOperacionFraccion
 {
 	public static void main (String args[])
 	{	
-		Fraccion fr1 = new Fraccion(10,8);
+		Fraccion fr1 = new Fraccion(5,4);
 		Fraccion fr2 = new Fraccion(2,5);
 		Fraccion resultado = OperacionFraccion.multiplica(fr1,fr2);
 		System.out.println("La multiplicacion de "+fr1+" * "+ fr2 + " = " + resultado);
